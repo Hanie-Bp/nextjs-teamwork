2446aacf13b9e042517f3808740ca7a9306d80b0
Hanie Bp <haniebaratpoor@gmail.com>
2025-01-08 12:55:09 +0330

    add footer codes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -48,6 +48,7 @@ export default function RootLayout({ children }) {
               {children}
             </Box>
           </main>
+          <Footer mode={mode}/>
         </body>
       </html>
     </ThemeProvider>
