db2dbd6b6fa5761db967ca349dfba6782d3f678f
Hanie Bp <haniebaratpoor@gmail.com>
2025-01-11 10:41:20 +0330

    add codes

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,33 +1,20 @@
-// "use client";
-// import React from "react";
-// import Button from "@mui/material/Button";
-
-// const DarkMode = ({ toggleTheme }) => {
-//   return (
-//     <Button variant="contained" sx={{ mt: 2 }} onClick={toggleTheme}>
-//       Toggle Mode
-//     </Button>
-//   );
-// };
-
-// export default DarkMode;
 "use client";
 import React, { useState } from "react";
 import WbSunnyIcon from "@mui/icons-material/WbSunny";
 import BedtimeIcon from "@mui/icons-material/Bedtime";
 import IconButton from "@mui/material/IconButton";
+import { useThemeContext } from "@/themeContext";
 
-const DarkMode = ({ toggleTheme }) => {
-  const [darkMode, setDarkMode] = useState(false);
+const DarkMode = () => {
+  const { isDarkMode, toggleTheme } = useThemeContext();
 
   const handleToggle = () => {
-    setDarkMode((prev) => !prev); 
     toggleTheme();
   };
 
   return (
     <IconButton onClick={handleToggle} sx={{ mt: 2 }}>
-      {darkMode ? (
+      {isDarkMode ? (
         <WbSunnyIcon sx={{ fontSize: 25, color: "white" }} />
       ) : (
         <BedtimeIcon sx={{ fontSize: 25, color: " grey[800]" }} />
@@ -37,4 +24,3 @@ const DarkMode = ({ toggleTheme }) => {
 };
 
 export default DarkMode;
-
