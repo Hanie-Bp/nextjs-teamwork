a77af5e5f4e96baedf9c60a3b3557eb2c09e9d0c
Hanie Bp <haniebaratpoor@gmail.com>
2025-01-14 12:40:49 +0330

    add actions

---
+++ src/utils/actions.js
@@ -0,0 +1,26 @@
+"use server";
+
+// import { revalidateTag } from "next/cache";
+
+export async function getData(url) {
+  const res = await fetch(url, { next: { tags: ["questions"] } });
+  const data = await res.json();
+  return data;
+}
+
+export async function patchData(url,data) {
+    await fetch(url, { method: "PATCH", body: JSON.stringify(data) });
+    revalidateTag("questions");
+    return data;
+  }
+  
+  export async function postData(url, data) {
+    await fetch(url, { method: "POST", body: JSON.stringify(data) });
+    revalidateTag("questions");
+  }
+  
+  export async function deleteData(url,) {
+    await fetch(url, { method: "DELETE" });
+    revalidateTag("questions");
+  }
+  
